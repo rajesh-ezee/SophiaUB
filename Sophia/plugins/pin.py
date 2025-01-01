31e8b08cb39fc184bc7d0edb8b2a267c9443c532
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-01 11:14:35 +0530

    Update pin.py

--- Sophia/plugins/pin.py
+++ Sophia/plugins/pin.py
@@ -4,18 +4,20 @@ from config import OWNER_ID as OWN
 from pyrogram import filters
 import asyncio
 import os
+from pyrogram.enums import ChatMemberStatus
 
 @Sophia.on_message(filters.command("pin", prefixes=HANDLER) & filters.user(OWN))
 async def pin_message(_, message):
     if message.reply_to_message:
         try:
+            a = await Sophia.get_chat_member(message.chat.id, message.from_user.id)
+            if a.status == ChatMemberStatus.MEMBER or not a.privileges.can_pin_messages:
+                return await message.reply("**You don't have enough admin rights to use this command ❌**")
             await Sophia.pin_chat_message(message.chat.id, message.reply_to_message_id, both_sides=True)
             await message.delete()
         except Exception as e:
-            if str(e) == """Telegram says: [400 CHAT_ADMIN_REQUIRED] - The method requires chat admin privileges (caused by "messages.UpdatePinnedMessage")""":
-                await message.reply("❌ You need admin access to do this!")
-            elif str(e).startswith("""Telegram says: [420 FLOOD_WAIT_X] - A wait of"""):
-                sec = int(e.split()[8])
+            if str(e).startswith("""Telegram says: [420 FLOOD_WAIT_X] - A wait of"""):
+                sec = int(str(e).split()[8])
                 await asyncio.sleep(sec)
                 await Sophia.pin_chat_message(message.chat.id, message.reply_to_message_id)
             else:
@@ -28,12 +30,14 @@ async def pin_message(_, message):
 async def unpin_message(_, message):
     if message.reply_to_message:
         try:
+            a = await Sophia.get_chat_member(message.chat.id, message.from_user.id)
+            if a.status == ChatMemberStatus.MEMBER or not a.privileges.can_pin_messages:
+                return await message.reply("**You don't have enough admin rights to use this command ❌**")
             await Sophia.unpin_chat_message(message.chat.id, message.reply_to_message_id)
+            await message.delete()
         except Exception as e:
-            if str(e) == """Telegram says: [400 CHAT_ADMIN_REQUIRED] - The method requires chat admin privileges (caused by "messages.UpdatePinnedMessage")""":
-                await message.reply("❌ You need admin access to do this!")
-            elif str(e).startswith("""Telegram says: [420 FLOOD_WAIT_X] - A wait of"""):
-                sec = int(e.split()[8])
+            if str(e).startswith("""Telegram says: [420 FLOOD_WAIT_X] - A wait of"""):
+                sec = int(str(e).split()[8])
                 await asyncio.sleep(sec)
                 await Sophia.unpin_chat_message(message.chat.id, message.reply_to_message_id)
             else:
