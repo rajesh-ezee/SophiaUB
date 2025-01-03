82d0aa283e8c664896508ac90c034c811b9b0e79
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-03 15:09:39 +0530

    Create cloner_communication.py

---
+++ Sophia/helpers/cloner_communication.py
@@ -0,0 +1,22 @@
+from Sophia import *
+from pyrogram import *
+import asyncio
+import aiofiles
+from variables import *
+
+# This function used for stop ub using SophiaCloner signel
+async def wait_for_stop_signel():
+  while not DEVELOPER_MODE:
+    try: 
+      async with aiofiles.open('communication.txt', 'r') as f:
+        c = await f.read()
+        if 'stop' in c:
+          await Sophia.send_message('me', "Stop signel received killing process..")
+          logging.warn("Stop signel received killing process..")
+          await Sophia.stop()
+          exit()
+        await asyncio.sleep(10)
+    except FileNotFoundError:
+      logging.info("Looks like userbot not deployed using SophiaCloner bot")
+      break
+    
