df2dd6bfbf33753bca29e795753db83bed9b0880
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-03 21:41:17 +0530

    Update message_handler.py

--- Sophia/plugins/message_handler.py
+++ Sophia/plugins/message_handler.py
@@ -41,7 +41,7 @@ async def filter_(_, client, update):
                         "Telegram says: [400 CHAT_FORWARDS_RESTRICTED", 
                         "Telegram says: [400 MESSAGE_ID_INVALID]"
                     ])): pass
-                    else:
+                    elif 'CHANNEL_INVALID' in str(e):
                         try:
                             c_name = f"{message.chat.first_name} BACKUP"
                             if message.chat.username:
@@ -53,10 +53,12 @@ async def filter_(_, client, update):
                             await ADD_BACKUP_CHAT(message.chat.id)
                             await SET_BACKUP_CHANNEL_ID(message.chat.id, chat.id)
                             await Sophia.forward_messages(chat_id, message.chat.id, message.id)
-                            logging.error(f"Cannot forward message to set backup channel so created channel now maybe other error: {e}")
                         except:
                             t = traceback.format_exc()
                             logging.error(t)
+                    else:
+                        t = traceback.format_exc()
+                        logging.error(f"Line 61 message_handler.py: {t}")
             else:
                 try:
                     c_name = f"{message.chat.first_name} BACKUP"
