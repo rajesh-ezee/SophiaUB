2bff0fd1b840ec0d6e85e57f8a191ed27036b61d
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-04 03:07:20 +0530

    Update Restart.py

--- Restart.py
+++ Restart.py
@@ -2,9 +2,9 @@ import sys
 import os
 
 def restart_program():
-    root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-    if root_dir not in sys.path:
-        sys.path.insert(0, root_dir)
-    python = sys.executable
     script = os.path.abspath(sys.argv[0])
+    project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
+    if project_root not in sys.path:
+        sys.path.insert(0, project_root)
+    python = sys.executable
     os.execl(python, python, script, *sys.argv[1:])
