fae18530958fb439f2ba091be8975d6536d5e4d7
Otazuki004 <>
2025-03-18 05:08:25 +0530

    Update

--- Sophia/plugins/restart.py
+++ Sophia/plugins/restart.py
@@ -5,14 +5,17 @@ from config import OWNER_ID as OWN
 from pyrogram import filters
 import asyncio
 import os
-
   
 @Sophia.on_message(filters.command(["restart", 'trestart'], prefixes=HANDLER) & filters.user(OWN))
 async def restart(_, message):
   if message.command[0] == 'trestart':
     try:
-      await message.edit("Set!")
-      await GetTime(" ".join(message.command[1:]), True)
-    except TypeError: return await message.reply("Nooo, this is not correct time format.\nUse: `.trestart 1h`")
+      txt = " ".join(message.command[1:])
+      if int(txt[:-1]) <= 5:
+        return await message.reply("Time should be greater than 5 sec.")
+      x = await GetTime(txt)
+      await message.edit(f"Done! userbot will be restarted in {txt}")
+      await asyncio.sleep(x)
+    except: return await message.edit("Nooo, this is not correct time format.\nUse: `.trestart 1h`")
   await message.edit("Restarting...")
   restart_program()
