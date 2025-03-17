7c3db123bebdfea9fb24fc2ef430beebfc6b0040
Otazuki004 <>
2025-03-18 02:54:26 +0530

    Update

--- Sophia/plugins/Urban_dictionary.py
+++ Sophia/plugins/Urban_dictionary.py
@@ -1,20 +1,20 @@
 from Sophia import HANDLER
-from Sophia.__main__ import Sophia as bot
+from Sophia.__main__ import Sophia as Sophia
 from config import OWNER_ID
 from pyrogram import filters
 import asyncio
 import os
 import requests
 
-@bot.on_message(filters.command("ud", prefixes=HANDLER) & filters.user(OWNER_ID))
+@Sophia.on_message(filters.command("ud", prefixes=HANDLER) & filters.user(OWNER_ID))
 async def urban_dictionary(_, message):
-        if len(message.command) < 2:
-             return await bot.send_message(message.chat.id, "where you input the text?")         
-        text = message.text.split(None, 1)[1]
-        try:
-          results = requests.get(
-            f'https://api.urbandictionary.com/v0/define?term={text}').json()
-          reply_text = f"""
+  if len(message.command) < 2:
+    return await message.reply("Please give an input of a word.\nExample: `.ud asap`")         
+  text = message.text.split(None, 1)[1]
+  try:
+    results = requests.get(
+      f'https://api.urbandictionary.com/v0/define?term={text}').json()
+    reply_text = f"""
 **Results for**: {text}
 
 **Defination**:
@@ -22,13 +22,13 @@ async def urban_dictionary(_, message):
 **Example:**
 {results["list"][0]["example"]}
 """
-        except Exception as e:
-              if str(e) == "list index out of range":
-                  await message.reply("I can't find it in Urban dictionary.")
-                  return
-              return await bot.send_message(message.chat.id, f"Somthing wrong Happens:\n`{e}`")
-        ud = await bot.send_message(message.chat.id, "Exploring....")
-        await ud.edit_text(reply_text)
+  except Exception as e:
+    if str(e) == "list index out of range":
+      await message.reply("Cannot find your query on Urban dictionary.")
+      return
+    return await Sophia.send_message(message.chat.id, f"Somthing wrong Happens:\n`{e}`")
+  ud = await Sophia.send_message(message.chat.id, "Exploring....")
+  await ud.edit_text(reply_text)
 
 MOD_NAME = "UD"
 MOD_HELP = ".ud <word> - To get definition of that word!"
--- Sophia/plugins/purge.py
+++ Sophia/plugins/purge.py
@@ -7,17 +7,15 @@ import logging
 
 @Sophia.on_message(filters.command("purge", prefixes=HANDLER) & filters.me)
 async def purge_messages(_, message):
-  if message.chat.type == ChatType.PRIVATE:
-    return await message.reply("This command only works in groups!")
   if not message.reply_to_message:
     return await message.reply("Reply to the message you want to delete from.")
   start = message.reply_to_message.id
   end = message.id
   for x in range(start, end + 1, 100):
     try:
-      x = list(range(x+1, x+101))
+      x = list(range(x, x+101))
       await Sophia.delete_messages(message.chat.id, x)
-      await asyncio.sleep(2)
+      await asyncio.sleep(3)
     except Exception as e:
       logging.error(f"Error deleting message {x}: {str(e)}")
   await message.reply("Purge complete.")
