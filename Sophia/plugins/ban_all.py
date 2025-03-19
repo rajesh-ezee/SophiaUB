b82e3555312d57ffc45f8e40fe46864b4aeb0069
Otazuki004 <>
2025-03-19 13:45:19 +0530

    Update

--- Sophia/plugins/ban_all.py
+++ Sophia/plugins/ban_all.py
@@ -1,105 +1,88 @@
-from Sophia import Sophia as bot
-from Sophia import HANDLER
+from Sophia import *
+import logging
 from config import OWNER_ID as OWN
-from pyrogram import enums
-from pyrogram import filters
+from pyrogram import *
 from pyrogram.types import *
 import asyncio
 
+info = logging.info
 async def is_admin(chat_id: int, user_id: int):
-    chat_members = await bot.get_chat_members(chat_id)
-    for member in chat_members:
-        if member.user.id == user_id:
-            return member.status in [enums.ChatMemberStatus.ADMINISTRATOR, enums.ChatMemberStatus.CREATOR]
-    return False
+  member = await Sophia.get_chat_members(chat_id)
+  return member.status in [enums.ChatMemberStatus.ADMINISTRATOR, enums.ChatMemberStatus.CREATOR]
 
-@bot.on_message(filters.command("unbanall", prefixes=HANDLER) & filters.user(OWN))
+@Sophia.on_message(filters.command("unbanall", prefixes=HANDLER) & filters.user(OWN))
 async def unbanall(_, message):
-    user_id = message.from_user.id
-    chat_id = message.chat.id
-    if user_id != OWN and (await is_admin(chat_id, user_id)) == False:
-        return
-    elif message.chat.type == enums.ChatType.PRIVATE:
-        return await message.reply("Sorry, This Command Only works in Groups!")
-    else:
+  user_id, chat_id = message.from_user.id, message.chat.id
+  if not await is_admin(chat_id, user_id):
+    return
+  elif message.chat.type == enums.ChatType.PRIVATE:
+    return await message.reply("Sorry, This Command Only works in Groups!")
+  else:
+    try:
+      BANNED, unban = [], 0
+      async for m in Sophia.get_chat_members(chat_id, filter=enums.ChatMembersFilter.BANNED):
+        BANNED.append(m.user.id)
         try:
-            BANNED = []
-            unban = 0
-            async for m in bot.get_chat_members(chat_id, filter=enums.ChatMembersFilter.BANNED):
-                BANNED.append(m.user.id)
-                try:
-                    await bot.unban_chat_member(chat_id, m.user.id)
-                    unban += 1
-                except: pass
-                await asyncio.sleep(0.8)
-            await message.reply("Found Banned Members: {}\nUnbanned Successfully: {}".format(len(BANNED), unban))
-        except Exception as e:
-            if "CHAT_ADMIN_REQUIRED" in str(e):
-                return await message.reply_text("**Sorry**, `I don't have Admin rights to do this!. ❌`")
-            await message.reply_text(f"**Error:** {e}")
-            print(e)
+          await Sophia.unban_chat_member(chat_id, m.user.id)
+          unban += 1
+        except: pass
+        await asyncio.sleep(1.2)
+      m, f = await message.reply("Found Banned Members: {}\nUnbanned Successfully: {}".format(len(BANNED), unban)), await msg.delete()
+    except Exception as e:
+      if "CHAT_ADMIN_REQUIRED" in str(e):
+        return await message.reply_text("**Sorry**, `I don't have Admin rights to do this!. ❌`")
+      await message.reply(f"**Error:** {e}")
+      info(e)
 
-@bot.on_message(filters.command("banall", prefixes=HANDLER) & filters.user(OWN))
+@Sophia.on_message(filters.command("banall", prefixes=HANDLER) & filters.user(OWN))
 async def banall(_, message):
-    chat_id = message.chat.id
-    user_id = message.from_user.id
-    if user_id != OWN and (await is_admin(chat_id, user_id)) == False:
-        return
-    elif message.chat.type == enums.ChatType.PRIVATE:
-        return await message.reply("This Command Only works in Groups!")
-    else:
+  user_id, chat_id = message.from_user.id, message.chat.id  
+  if not await is_admin(chat_id, user_id):
+    return
+  elif message.chat.type == enums.ChatType.PRIVATE:
+    return await message.reply("This Command Only works in Groups!")
+  else:
+    try:
+      Members, Admins = [], []
+      async for x in Sophia.get_chat_members(chat_id):
+        if not x.privileges: Members.append(x.user.id)
+        else: Admins.append(x.user.id)
+      for user_id in Members:
         try:
-            Members = []
-            Admins = []
-            async for x in bot.get_chat_members(chat_id):
-                if not x.privileges:
-                    Members.append(x.user.id)
-                else:
-                    Admins.append(x.user.id)
-            for user_id in Members:
-                if message.text.split()[0].lower().startswith("s"):
-                    m = await bot.ban_chat_member(chat_id, user_id)
-                    await m.delete()
-                else:
-                    await bot.ban_chat_member(chat_id, user_id)
-            await message.reply_text("Successfully Banned: {}\nRemaining Admins: {}".format(len(Members), len(Admins)))
-        except Exception as e:
-            if "400 CHAT_ADMIN_REQUIRED" in str(e):
-                return await message.reply_text("You don't have enough admin rights to do this 🚫")
-            await message.reply_text(f"**Error:** {e}")
-            print(e)
+          await Sophia.ban_chat_member(chat_id, user_id)
+          await Sophia.unban_chat_member(chat_id, user_id)
+          await asyncio.sleep(1.2)
+        except Exception: info(Exception)
+      m, f = await message.reply("Successfully Banned: {}\nRemaining Admins: {}".format(len(Members), len(Admins))), await msg.delete()
+    except Exception as e:
+      await message.reply_text(f"**Error:** {e}")
+      info(e)
 
-@bot.on_message(filters.command("kickall", prefixes=HANDLER) & filters.user(OWN))
+@Sophia.on_message(filters.command("kickall", prefixes=HANDLER) & filters.user(OWN))
 async def kickall(_, message):
-    chat_id = message.chat.id
-    user_id = message.from_user.id
-    if user_id != OWN and (await is_admin(chat_id, user_id)) == False:
-        return
-    elif message.chat.type == enums.ChatType.PRIVATE:
-        return await message.reply("This Command Only works in Groups!")
-    else:
+  user_id, chat_id = message.from_user.id, message.chat.id
+  if not await is_admin(chat_id, user_id):
+    return
+  elif message.chat.type == enums.ChatType.PRIVATE:
+    return await message.reply("This Command Only works in Groups!")
+  else:
+    try:
+      Admins, Members = [], []
+      async for x in Sophia.get_chat_members(chat_id):
+        if not x.privileges: Members.append(x.user.id)
+        else: Admins.append(x.user.id)
+      for user_id in Members:
         try:
-            Members = []
-            Admins = []
-            async for x in bot.get_chat_members(chat_id):
-                if not x.privileges:
-                    Members.append(x.user.id)
-                else:
-                    Admins.append(x.user.id)
-            for user_id in Members:
-                if message.text.split()[0].lower().startswith("s"):
-                    m = await bot.ban_chat_member(chat_id, user_id)
-                    await bot.unban_chat_member(chat_id, user_id)
-                    await m.delete()
-                else:
-                    await bot.ban_chat_member(chat_id, user_id)
-                    await bot.unban_chat_member(chat_id, user_id)
-            await message.reply_text("Successfully Kicked: {}\nRemaining Admins: {}".format(len(Members), len(Admins)))
-        except Exception as e:
-            if "CHAT_ADMIN_REQUIRED" in str(e):
-                return await message.reply_text("You don't have enough admin rights to do this 🚫")
-            await message.reply_text(f"**Error:** {e}")
-            print(e)
+          await Sophia.ban_chat_member(chat_id, user_id)
+          await Sophia.unban_chat_member(chat_id, user_id)
+          await asyncio.sleep(1.2)
+        except Exception: info(Exception)
+      m, f = await message.reply_text("Successfully Kicked: {}\nRemaining Admins: {}".format(len(Members), len(Admins))), await msg.delete()
+    except Exception as e:
+      if "CHAT_ADMIN_REQUIRED" in str(e):
+        return await message.reply_text("You don't have enough admin rights to do this 🚫")
+      await message.reply_text(f"**Error:** {e}")
+      info(e)
 
 MOD_NAME = 'Bans'
 MOD_HELP = """**⚕️ Banall**
