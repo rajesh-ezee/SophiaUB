7036fcbbb6beeb0436674bf49eafc0e6bce4f500
Otazuki004 <>
2025-03-20 18:24:23 +0530

    Update

--- Sophia/plugins/pin.py
+++ Sophia/plugins/pin.py
@@ -1,52 +1,28 @@
-from Sophia import HANDLER
-from Sophia.__main__ import Sophia
+from Sophia import *
 from config import OWNER_ID as OWN
-from pyrogram import filters
+from pyrogram import *
 import asyncio
-import os
-from pyrogram.enums import ChatMemberStatus
+from pyrogram.enums import *
 
-@Sophia.on_message(filters.command("pin", prefixes=HANDLER) & filters.user(OWN))
-async def pin_message(_, message):
-    if message.reply_to_message:
-        try:
-            a = await Sophia.get_chat_member(message.chat.id, message.from_user.id)
-            if a.status == ChatMemberStatus.MEMBER or not a.privileges.can_pin_messages:
-                return await message.reply("**You don't have enough admin rights to use this command ❌**")
-            await Sophia.pin_chat_message(message.chat.id, message.reply_to_message_id, both_sides=True)
-            await message.delete()
-        except Exception as e:
-            if str(e).startswith("""Telegram says: [420 FLOOD_WAIT_X] - A wait of"""):
-                sec = int(str(e).split()[8])
-                await asyncio.sleep(sec)
-                await Sophia.pin_chat_message(message.chat.id, message.reply_to_message_id)
-            else:
-                await message.reply(f"**Error:** `{e}`")
+@Sophia.on_message(filters.command(["pin", "unpin"], prefixes=HANDLER) & filters.user(OWN))
+async def pin_unpin(_, message):
+  if not message.reply_to_message:
+    return await message.reply("ℹ️ Please reply to a message to pin/unpin")
+  chat = message.chat
+  if chat.type != ChatType.PRIVATE:
+    member = await Sophia.get_chat_member(chat.id, message.from_user.id)
+    if member.status == ChatMemberStatus.MEMBER or not member.privileges.can_pin_messages:
+      return await message.reply("**You don't have enough admin rights to use this command ❌**")
+  action = Sophia.pin_chat_message if message.command[0] == "pin" else Sophia.unpin_chat_message
+  try:
+    await action(chat.id, message.reply_to_message.id, both_sides=True if message.command[0] == "pin" else None)
+    await message.delete()
+  except Exception as e:
+    if "FLOOD_WAIT" in str(e):
+      await asyncio.sleep(int(str(e).split()[8]))
+      await action(chat.id, message.reply_to_message.id)
     else:
-        await message.reply("ℹ️ Please reply to a message to unpin")
-
-
-@Sophia.on_message(filters.command("unpin", prefixes=HANDLER) & filters.user(OWN))
-async def unpin_message(_, message):
-    if message.reply_to_message:
-        try:
-            a = await Sophia.get_chat_member(message.chat.id, message.from_user.id)
-            if a.status == ChatMemberStatus.MEMBER or not a.privileges.can_pin_messages:
-                return await message.reply("**You don't have enough admin rights to use this command ❌**")
-            await Sophia.unpin_chat_message(message.chat.id, message.reply_to_message_id)
-            await message.delete()
-        except Exception as e:
-            if str(e).startswith("""Telegram says: [420 FLOOD_WAIT_X] - A wait of"""):
-                sec = int(str(e).split()[8])
-                await asyncio.sleep(sec)
-                await Sophia.unpin_chat_message(message.chat.id, message.reply_to_message_id)
-            else:
-                await message.reply(f"**Error:** `{e}`")
-    else:
-        await message.reply("ℹ️ Please reply to a message to unpin")
-        
+      await message.reply(f"**Error:** `{e}`")
 
 MOD_NAME = 'Pin'
 MOD_HELP = ".pin (reply) - To pin the replied message!\n.unpin (reply) - To unpin the replied message!"
-
-# Rewrite soonnnn!!!! @DevsBase™
---
+++ Sophia/plugins/silent.py
@@ -0,0 +1,29 @@
+from pyrogram import *
+from Sophia import *
+from ..Database.silent import Silent
+import asyncio
+
+silent = Silent()
+
+async def SilentFilter(_, __, m):
+  if await silent.get() & m.chat.id not in await silent.get_exceptions():
+    try:
+      await Sophia.read_chat_history(m.chat.id)
+      await Sophia.read_mentions(m.chat.id)
+      await Sophia.read_reactions(m.chat.id)
+    except: pass
+
+@Sophia.on_message(filters.command('silent') & filters.me)
+async def silent(_, m):
+  x = await silent.get()
+  if x:
+    await silent.off()
+    return await m.reply("Ilovedher")
+  else:
+    await silent.on()
+    return await m.reply("True")
+    
+@app.on_message(~filters.me & filters.create(SilentFilter))
+@app.on_message_reaction(~filters.me & filters.create(SilentFilter))
+async def do_ntg(_, __):
+  pass
