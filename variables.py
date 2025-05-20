a451ec0f252858c5b1bb9e3948850c7c0c2d3ca0
RAJESH S <87179096+rajesh-ezee@users.noreply.github.com>
2025-05-20 12:25:38 +0530

    variables.py

--- variables.py
+++ variables.py
@@ -1,11 +1,11 @@
 # IF YOU CAN'T USE ENV VARIABLES FILL EVERYTHING HERE
 
-VAR_SESSION = ""
-VAR_API_ID = "10187126"
-VAR_API_HASH = "ff197c0d23d7fe54c89b44ed092c1752"
-VAR_MONGO_DB_URI = "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority"
-VAR_TOKEN = ""
-VAR_REPO_URL = "https://github.com/Otazuki004/SophiaUB/" # Change to your fork repo
+VAR_SESSION = "BQGrb00AYPd624hkA8_vPHzvp9xvfwQE7VF6UiI0q6AD45zxqT_rv3s0N3cTXUmNKXfjyK9v_jCZhrprXbY9FXTTKUV26-o5rPZe5LGMgFgtFBpjpv-sd5f7JrESnWIPqs_wZG4PDLgh0iBrWf50qlCcPO0YC-Cah-KMORD4VbgzrGUKMZl8hyuKFjK_GkqEbew2b_PA763CsFEQRbzdL9ce49Z2FRPUnMCobOpkwQNa0hIqhS936rphXAvRs_L9v6P-DzxGTZ4RmdDnaGKxs5vfGExty1XwRgR4jMRi7jPUm2MkRErFmafoarrhT-mnEmtS2HNAub5VGfqUON_WYBzoTdVOJwAAAAHhGt_OAA"
+VAR_API_ID = "28012365"
+VAR_API_HASH = "40db55020f80e832af2404f81df3e1b4"
+VAR_MONGO_DB_URI = "mongodb+srv://heartbeat:Beat7Heart@heartbeat.1h1nbxv.mongodb.net/?retryWrites=true&w=majority"
+VAR_TOKEN = "7068876137:AAH-__dcUUYXs2c02mf9yapnWhCaI-J8i4Q"
+VAR_REPO_URL = "https://github.com/rajesh-ezee/SophiaUB/" # Change to your fork repo
 DEVELOPER_MODE = True
 
 # Use "" for strings
